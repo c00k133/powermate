592931553e510f45e112a03ae9abd282a4472dc5
Axel Neergaard <axel.neergaard@aalto.fi>
2018-01-29 00:29:44 +0200

    Own file for personal needs

---
+++ volume.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from __future__ import division
+
+import glob, powermate, alsaaudio
+
+
+class VolPowerMate(powermate.PowerMateBase):
+    def __init__(self, path):
+        super(VolPowerMate, self).__init__(path)
+        self.__m = alsaaudio.Mixer()
+        self.__globvol = 25
+        self.__m.setvolume(self.__globvol)
+        self.__mute = False
+
+    def rotate(self, rotation):
+        vol = int(self.__m.getvolume()[0])
+        if not self.__mute:
+            self.__globvol = min(100, max(0, vol + rotation))
+            self.__m.setvolume(self.__globvol)
+            return powermate.LedEvent.percent(self.__globvol / 100)
+        else:
+            return powermate.LedEvent.pulse()
+
+    def short_press(self):
+        self.__mute = not self.__mute
+        if self.__mute:
+            self.__m.setvolume(0)
+            return powermate.LedEvent.pulse()
+        else:
+            sefl.__m.setvolume(self.__globvol)
+            return powermate.LedEvent.percent(self.globvol / 100)
+
+
+if __name__ == '__main__':
+    pm = VolPowerMate(glob.glob('/dev/input/by-id/*PowerMate*')[0])
+    pm.run()
+
