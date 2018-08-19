a4580942ec792fca1f95f86f16d692b2dbef3d36
Axel Neergaard <axel.neergaard@aalto.fi>
2018-08-20 01:15:28 +0300

    Refactores class

--- volume.py
+++ volume.py
@@ -1,38 +1,54 @@
 #!/usr/bin/env python3
 
-from __future__ import division
 
+from __future__ import division
 import glob, powermate, alsaaudio
 
 
 class VolPowerMate(powermate.PowerMateBase):
-    def __init__(self, path):
+
+    def __init__(self, path, alsa_mixer=None):
         super(VolPowerMate, self).__init__(path)
-        self.__m = alsaaudio.Mixer()
-        self.__globvol = 25
-        self.__m.setvolume(self.__globvol)
-        self.__mute = False
+        self._m = self.getmixer(alsa_mixer)
+
+    def getmixer(self, alsa_mixer):
+        if alsa_mixer:
+            return alsaaudio.Mixer(alsa_mixer)
+        else:
+            return alsaaudio.Mixer()
+
+    def getvolume(self):
+        left, right = self._m.getvolume()
+        return (left + right) / 2
+
+    def getmute(self):
+        left, right = self._m.getmute()
+        return left or right
+
+    def setvolume(self, vol):
+        self._m.setvolume(vol)
+
+    def setmute(self, mute):
+        self._m.setmute(mute)
 
     def rotate(self, rotation):
-        vol = int(self.__m.getvolume()[0])
-        if not self.__mute:
-            self.__globvol = min(100, max(0, vol + rotation))
-            self.__m.setvolume(self.__globvol)
-            return powermate.LedEvent.percent(self.__globvol / 100)
+        if not self.getmute():
+            vol = min(100, max(0, self.getvolume() + rotation))
+            self.setvolume(int(vol))
+            return powermate.LedEvent.percent(vol / 100)
         else:
             return powermate.LedEvent.pulse()
 
     def short_press(self):
-        self.__mute = not self.__mute
-        if self.__mute:
-            self.__m.setvolume(0)
-            return powermate.LedEvent.pulse()
+        if self.getmute():
+            self.setmute(0)
+            return powermate.LedEvent.percent(self.getvolume() / 100)
         else:
-            self.__m.setvolume(self.__globvol)
-            return powermate.LedEvent.percent(self.globvol / 100)
+            self.setmute(1)
+            return powermate.LedEvent.pulse()
 
 
 if __name__ == '__main__':
-    pm = VolPowerMate(glob.glob('/dev/input/by-id/*PowerMate*')[0])
+    mixer = 'Headphone'
+    pm = VolPowerMate(glob.glob('/dev/input/by-id/*PowerMate*')[0], mixer)
     pm.run()
-
